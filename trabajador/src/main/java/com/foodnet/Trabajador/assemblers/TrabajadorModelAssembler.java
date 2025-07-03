247d0288ae8af52c48ad1834876701a270cdc9ba
Maryori <mary.gutierrez@duocuc.cl>
2025-07-03 18:44:29 -0400

    agrega configuraciones hateoas con trabajador

--- trabajador/src/main/java/com/foodnet/Trabajador/assemblers/TrabajadorModelAssembler.java
+++ trabajador/src/main/java/com/foodnet/Trabajador/assemblers/TrabajadorModelAssembler.java
@@ -1,4 +1,21 @@
 package com.foodnet.Trabajador.assemblers;
 
-public class TrabajadorModelAssembler {
+
+import com.foodnet.Trabajador.Controller.TrabajadorControllerV2;
+import com.foodnet.Trabajador.Model.Trabajador;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class TrabajadorModelAssembler implements RepresentationModelAssembler<Trabajador, EntityModel<Trabajador>> {
+    @Override
+    public EntityModel<Trabajador> toModel(Trabajador trabajador) {
+        return EntityModel.of(trabajador,
+                linkTo(methodOn(TrabajadorControllerV2.class).getTrabajadorById(trabajador.getId_trabajador())).withSelfRel(),
+                linkTo(methodOn(TrabajadorControllerV2.class).getAllTrabajador()).withRel("clientes"));
+    }
 }
