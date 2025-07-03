d8458d647091391ff9b5891bdb5c309c2cdca778
millaray <mi.tobar@duocuc.cl>
2025-07-02 20:42:52 -0400

    agrega configuraciones hateoas con cliente

--- cliente/src/main/java/com/foodnet/Cliente/assemblers/ClienteModelAssembler.java
+++ cliente/src/main/java/com/foodnet/Cliente/assemblers/ClienteModelAssembler.java
@@ -1,5 +1,19 @@
 package com.foodnet.Cliente.assemblers;
 
+import com.foodnet.Cliente.Controller.ClienteControllerV2;
+import com.foodnet.Cliente.Model.Cliente;
+import org.springframework.stereotype.Component;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
 
-public class ClienteModelAssembler {
+@Component
+public class ClienteModelAssembler implements   RepresentationModelAssembler  <Cliente, EntityModel <Cliente>> {
+    @Override
+    public EntityModel<Cliente> toModel(Cliente cliente) {
+        return EntityModel.of(cliente,
+                linkTo(methodOn(ClienteControllerV2.class).getClienteById(cliente.getId_cliente())).withSelfRel(),
+                linkTo(methodOn(ClienteControllerV2.class).getAllClientes()).withRel("clientes"));
+    }
 }
+
